3fa83d4fb3cc0e293125f0837c7639577b5c9d45
JangYeonSeo <turbo4342@gmail.com>
2021-08-25 16:40:48 +0900

    7교시

--- spring/src/main/java/com/human/app/HomeController.java
+++ spring/src/main/java/com/human/app/HomeController.java
@@ -24,28 +24,32 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping("/membersName")
-	public String home1(Locale locale, Model m) {
-		m.addAttribute("m_name", "Jenny,Jisoo");
-		return "members";
-		
+//	@RequestMapping("/membersName")
+//	public String home1(Locale locale, Model m) {
+//		m.addAttribute("m_name", "Jenny,Jisoo");
+//		return "members";
+//		
+//	}
+//	@RequestMapping("/test")
+//	public String home(Locale locale, Model model) {
+//		logger.info("Welcome home! The client locale is {}.", locale);
+//		
+//		Date date = new Date();
+//		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+//		
+//		String formattedDate = dateFormat.format(date);
+//		
+//		model.addAttribute("serverTime", formattedDate );
+//		model.addAttribute("myName","Jang Yeon Seo");
+//		model.addAttribute("homeAddress","불당동");
+//		model.addAttribute("mobileNumber","010");
+//		return "home";
+//	}
+//	
+	@RequestMapping("/getinfo")
+	public String getInfo() {
+		return "getinfo";
 	}
-	@RequestMapping("/test")
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		model.addAttribute("myName","Jang Yeon Seo");
-		model.addAttribute("homeAddress","불당동");
-		model.addAttribute("mobileNumber","010");
-		return "home";
-	}
-	
 	@RequestMapping("/info")
 	public String doInfo(HttpServletRequest hsr, Model model) {
 		String uid = hsr.getParameter("userid");
---
+++ spring/src/main/webapp/WEB-INF/views/getinfo.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Get info</title>
+</head>
+<body>
+<form method = "get" action = "/app/info">
+	USERID : <input type="text" name="userid"><br>
+	ADDRESS : <input type="text" name="address"><br><br>
+	<input type="submit" name="전송">
+</form>
+</body>
+</html>
