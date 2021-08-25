443308e3839baf3d60e62f65c310cbf31b2d54dd
JangYeonSeo <turbo4342@gmail.com>
2021-08-25 13:58:02 +0900

    5교시

--- spring/src/main/java/com/human/app/MyController.java
+++ spring/src/main/java/com/human/app/MyController.java
@@ -1,8 +1,16 @@
 package com.human.app;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MyController {
-
+	
+	@RequestMapping(value="/contactus",method=RequestMethod.GET)
+	public String method1(Model model) {
+		model.addAttribute("mobile", "....");
+		return "contactus";
+	}
 }
---
+++ spring/src/main/webapp/WEB-INF/views/contactus.jsp
@@ -0,0 +1,14 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>myCompany</title>
+</head>
+<body>
+	<p>저희 회사 연락처는 ${mobile}입니다.</p>
+</body>
+</html>
